4ced4198a732527e4bef345275d3d9ba8ec32e52
Ayan Huda <ayanhudda05@gmail.com>
2025-02-13 02:25:11 -0600

    tried using cmake and google test but it did not work at all.

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/ayanh/Downloads/cpsc2376-Ayan/tests
+# Build directory: C:/Users/ayanh/Downloads/cpsc2376-Ayan/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- tests/practice03_test/practice03_test.cpp
+++ tests/practice03_test/practice03_test.cpp
@@ -1,23 +1,26 @@
-#include "gtest/gtest.h"
-#include "practice03.cpp"
+#include <gtest/gtest.h>
+#include "../practice/practice03/practice03.cpp"
 
-TEST(MathUtilsTest, SumRange) {
-    ASSERT_EQ(MathUtils::sumRange(1, 5), 10);  
-    ASSERT_EQ(MathUtils::sumRange(5, 5), 0);  
+TEST(MathUtilsTest, SumRange)
+{
+    ASSERT_EQ(MathUtils::sumRange(1, 5), 10);
+    ASSERT_EQ(MathUtils::sumRange(5, 5), 0);
 }
 
-TEST(MathUtilsTest, ContainsNegative) {
+TEST(MathUtilsTest, ContainsNegative)
+{
     std::vector<int> numbers1 = {3, -1, 5, 7};
     std::vector<int> numbers2 = {3, 1, 5, 7};
 
-    ASSERT_TRUE(MathUtils::containsNegative(numbers1)); 
-    ASSERT_FALSE(MathUtils::containsNegative(numbers2)); 
+    ASSERT_TRUE(MathUtils::containsNegative(numbers1));
+    ASSERT_FALSE(MathUtils::containsNegative(numbers2))
 }
 
-TEST(MathUtilsTest, FindMax) {
+TEST(MathUtilsTest, FindMax)
+{
     std::vector<int> numbers1 = {3, -1, 5, 7};
     std::vector<int> numbers2 = {3, 3, 3, 3};
 
-    ASSERT_EQ(MathUtils::findMax(numbers1), 7); 
-    ASSERT_EQ(MathUtils::findMax(numbers2), 3); 
+    ASSERT_EQ(MathUtils::findMax(numbers1), 7);
+    ASSERT_EQ(MathUtils::findMax(numbers2), 3);
 }
