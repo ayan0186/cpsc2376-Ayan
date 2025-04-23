830393b069a6c8af34461af9bde47ac3b5d3297f
Ayan Huda <ayanhudda05@gmail.com>
2025-04-23 02:10:05 -0500

    added a working gui with a bouncing triangle

--- practice/practice12/bounce.cpp
+++ practice/practice12/bounce.cpp
@@ -1,22 +1,26 @@
+#define SDL_MAIN_HANDLED
 #include <SDL.h>
 #include <iostream>
- 
-int main() {
+
+int main()
+ {
+    std::cout << "Bouncing rectangle build...\n";
+
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
         return 1;
     }
- 
+
     SDL_Window* window = SDL_CreateWindow("Bouncing Rectangle",
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
- 
+
     SDL_Rect rect = {100, 100, 50, 50};
     int dx = 4, dy = 3;
- 
+
     bool running = true;
     SDL_Event e;
- 
+
     while (running) {
         while (SDL_PollEvent(&e)) {
             if (e.type == SDL_QUIT)
@@ -24,23 +28,23 @@ int main() {
             if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE)
                 running = false;
         }
- 
+
         rect.x += dx;
         rect.y += dy;
- 
+
         if (rect.x < 0 || rect.x + rect.w > 800) dx *= -1;
         if (rect.y < 0 || rect.y + rect.h > 600) dy *= -1;
- 
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // black background
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
- 
-        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // red rectangle
+
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
         SDL_RenderFillRect(renderer, &rect);
- 
+
         SDL_RenderPresent(renderer);
-        SDL_Delay(16); // ~60 FPS
+        SDL_Delay(16);
     }
- 
+
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
